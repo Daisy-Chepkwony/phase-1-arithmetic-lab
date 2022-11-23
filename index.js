00e7ef23a30f3e209c2005f8065a62089a6ed7de
+ <+>
2022-11-23 14:27:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+ let num1=31;
+ let num2=2;
+ let multiply=num1*num2;
+
+ 
+ let random=1;
+ Math.random()*10;
+
+ let num3=10
+ let num4=6
+
+ let mod =10%6 ;
+
+ let max=Math.max(20,5,18);
